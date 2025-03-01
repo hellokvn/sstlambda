c5c9974fd52eaeba0a0490b3e0f9d1bb42f89a90
Kevin <hellokevinvogel@gmail.com>
2025-03-01 21:14:57 +0700

    doesnt work: Could not resolve

--- src/api.ts
+++ src/api.ts
@@ -1,8 +1,9 @@
 import { Handler } from "aws-lambda";
+import { v4 } from "uuid";
 
 export const handler: Handler = async (_event) => {
   return {
     statusCode: 200,
-    body: `Hello World!`,
+    body: `Hello World! ${v4()}`,
   };
 };
